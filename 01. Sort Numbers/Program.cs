ac9750a540647686e16ac10bf51aba414d5f5204
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-21 13:27:02 +0200

    Add files via upload

---
+++ 01. Sort Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < 3; i++)
+{
+	if (n1 >= n2 && n1 >= n3)
+	{
+		Console.WriteLine(n1);
+		n1 = int.MinValue;
+	}
+	else if (n2 >= n1 && n2 >= n3)
+	{
+		Console.WriteLine(n2);
+		n2 = int.MinValue;
+	}
+	else if (n3 >= n1 && n3 >= n2)
+	{
+		Console.WriteLine(n3);
+		n3 = int.MinValue;
+	}
+}
